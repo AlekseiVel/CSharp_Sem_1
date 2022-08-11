1f532a01bdf0ac464fdde96057ef5a1a3af61be2
«AlekseiVel» <velmezov.au@gmail.com>
2022-08-11 21:22:52 +0300

    Ex_4_Hometask ver1

---
+++ Ex_4_Hometask/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три 
+// числа и выдаёт максимальное из этих чисел.
+
+int max = 0, numberA = 0, numberB = 0, numberC = 0;
+
+Console.Write("Введите первое число: ");
+numberA = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+numberB = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите третье число: ");
+numberC = int.Parse(Console.ReadLine()!);
+
+if(numberA > max) max = numberA;
+if(numberB > max) max = numberB;
+if(numberC > max) max = numberC;
+
+Console.WriteLine($"Максимальное число из трех: {max}");
