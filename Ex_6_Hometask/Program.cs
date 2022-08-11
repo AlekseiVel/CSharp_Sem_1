0aa4d39887de83c001041faae8c58e38159fdfe6
«AlekseiVel» <velmezov.au@gmail.com>
2022-08-11 21:36:51 +0300

    Ex_6_Hometask ver1

---
+++ Ex_6_Hometask/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+int number = 0;
+
+Console.Write("Введите число для проверки: ");
+number = int.Parse(Console.ReadLine()!);
+
+if(number % 2 == 0)
+{
+    Console.WriteLine($"Число {number} - четное.");
+}
+else
+{
+    Console.WriteLine($"Число {number} - нечетное.");
+}
