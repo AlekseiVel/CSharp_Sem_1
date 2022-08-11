cf3009c007616845162b27a084848543e0185cb1
«AlekseiVel» <velmezov.au@gmail.com>
2022-08-11 21:08:19 +0300

    Ex_2 Homework is done

---
+++ Ex_2_Hometask/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа 
+// и выдаёт, какое число большее, а какое меньшее.
+
+int max = 0;
+int numberA = 0;
+int numberB = 0;
+
+Console.Write("Введите первое число: ");
+numberA = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+numberA = int.Parse(Console.ReadLine()!);
+
+if(numberA > max) max = numberA;
+if(numberB > max) max = numberB;
+
+Console.WriteLine($"Большее число - {max}");
