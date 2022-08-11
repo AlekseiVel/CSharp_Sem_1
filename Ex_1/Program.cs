5539d51859ffa4dc23bd3026e94443740363155b
«AlekseiVel» <velmezov.au@gmail.com>
2022-08-11 14:54:35 +0300

    Ex_1 is done

---
+++ Ex_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// напишите программу, которая на вход принимает два числа
+// и проверяет, является ли второе число квадратом первого
+
+Console.WriteLine("Введите первое число");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int numberB = int.Parse(Console.ReadLine());
+
+if(numberB == numberA * numberA)
+Console.WriteLine("Да!");
+
+if(numberB != numberA * numberA)
+Console.WriteLine("Нет!");
