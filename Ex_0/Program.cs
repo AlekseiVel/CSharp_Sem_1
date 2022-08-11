b7383f34ff96a633d377e906fc84bcadb3ed8f8c
«AlekseiVel» <velmezov.au@gmail.com>
2022-08-11 14:40:09 +0300

    Ex_0 is done

---
+++ Ex_0/Program.cs
@@ -0,0 +1,10 @@
+﻿// напишите программу, которая на вход принимает число
+// и выдает его квадрат
+
+Console.WriteLine("Введите число квадрат которого надо найти");
+int number = int.Parse(Console.ReadLine());
+
+// int sqr = number*number;
+double sqr = Math.Pow(number, 2);
+
+Console.WriteLine($"Квадрат числа {number} равен:  {sqr}");
