207efcea69411b4a30d1eff16931feb52e2989bf
«AlekseiVel» <velmezov.au@gmail.com>
2022-08-12 21:26:48 +0300

    Ex_8_ver2

--- Ex_8_Hometask/Program.cs
+++ Ex_8_Hometask/Program.cs
@@ -1,14 +1,14 @@
 ﻿//Задача 8: Напишите программу, которая на вход принимает число (N), 
 // а на выходе показывает все чётные числа от 1 до N.
 
-int i = 0;
+int i = 2;
 int number = 0;
 
 Console.Write("Введите число: ");
 number = int.Parse(Console.ReadLine()!);
 
-while (i <= number - 3)
+while (i <= number)
 {
-    i += 2;
-    Console.Write($"{i} ");
+        Console.Write($"{i} ");
+        i += 2;
 }
